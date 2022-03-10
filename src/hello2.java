5a8701473587838e6d4646f10e0cd9e5203b4b8e
meenzeechae <wendy5013@naver.com>
2022-03-10 09:58:48 +0900

    [JAVA] hello .java 생성
    Hello World 출력

---
+++ src/hello2.java
@@ -0,0 +1,16 @@
+
+/*
+ * 첫번째 예제프로그램 
+ * @version 1.0 2022-03-10
+ * @author 채민지
+ */
+public class hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Hello World 출력
+		System.out.println("Hello World!");
+		System.out.println("I'm a new Java programmer.");
+	}
+
+}
