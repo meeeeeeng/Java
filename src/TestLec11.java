4a7f453db3bba246d1e26bba2a9dac39090feb42
meenzeechae <wendy5013@naver.com>
2022-05-19 10:32:42 +0900

    [JAVA] 11주자 수업

---
+++ src/TestLec11.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class TestLec11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+			try{System.out.println(readString());
+			}
+			catch(IOException e){
+				System.out.println("에러 발생");
+				}
+	}
+	public static String readString() throws IOException {
+		
+		byte[] buf=new byte[100];
+		System.out.println("문자열을 입력하시오: ");
+		throw IOException; //System.in.read(buf);
+		return new String(buf);
+		
+		
+	}
+
+}
